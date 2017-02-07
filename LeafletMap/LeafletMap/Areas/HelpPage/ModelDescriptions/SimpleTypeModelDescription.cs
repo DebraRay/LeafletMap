4d8582de2a43787c35bc3d9f227a0b75446193d7
Debra Ray <debrarayco@gmail.com>
2017-02-07 14:32:02 -0700

    Initial Commit.

---
+++ LeafletMap/LeafletMap/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LeafletMap.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LeafletMap/LeafletMap/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeafletMap.Models
+{
+    // Models returned by MeController actions.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
